f65c39fe7750821d1fe871122edaf539433dfdad
brunoken22 <bruno_am_22@hotmail.com>
2023-07-17 21:56:02 -0300

    todo ok .

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import {useOrder, useProduct} from '@/lib/hooks'
 
-export  default async  function ProductId({params}:any){
+export  default   function ProductId({params}:any){
    const router = useRouter()
    const [product,setProduct] =useState({})
    const data = useProduct("/api/products/"+params.id)
@@ -19,8 +19,7 @@ export  default async  function ProductId({params}:any){
 
    const handleClick = async(e:FormEvent)=>{
       e.preventDefault()
-   const orderResData=await useOrder(localStorage?.getItem("token")||"",params.id)
-      
+      const orderResData=await useOrder(localStorage?.getItem("token")||"",params.id)
       console.log(orderResData)
       if(orderResData?.url){
          window.open(orderResData.url,'_blank');
@@ -40,11 +39,11 @@ export  default async  function ProductId({params}:any){
                <Subtitle>${data?data["Unit cost"]:null}</Subtitle>
             </div>   
             <Button variant="contained" onClick={handleClick}>Comprar</Button>
-            <div style={{textAlign:"start",display: "flex",flexDirection:"column",rowGap:" 1rem",alignItems:"start",paddingLeft:"1rem"}}>
+            <div style={{textAlign:"start",display: "flex",flexDirection:"column",rowGap:" 1rem",alignItems:"center",paddingLeft:"1rem"}}>
                <Body $bg="#169f4e" $style={{"align-items": "center",display: "flex",gap: "0.2rem"}}><CheckCircleOutlineIcon/>{data?data["In stock"]?"Stock disponible":null:null} </Body>
                <Body $bg="#000"><strong> Sobre este producto: </strong>{data?data.Description:null}</Body>
             </div>
          </div>
-   </div>
+      </div>
    )
 }
