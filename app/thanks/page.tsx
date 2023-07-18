48e6beaccb08306f03e459684a9f700a0e715e2b
brunoken22 <bruno_am_22@hotmail.com>
2023-07-17 21:01:27 -0300

    thanks

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -1,1 +1,16 @@
-export default function Hola(){}
+'use client'
+import {Title} from "@/ui/typography"
+import Button from "@mui/material/Button"
+import Image from "next/image"
+import Link from "next/link"
+import gif from '@/giphy.gif'
+export default function Thanks(){
+   return (
+      <div style={{textAlign:"center",marginTop:"20%"}}>
+         <Image src={gif} alt="Gracias" height={100} />
+         <Title $bg="#000">Muchas gracias por tu compra!</Title>
+         <h4 style={{marginTop:"2rem",marginBottom:"2.5rem"}}>En la brevedad le llegara mas informacion sobre el envío</h4>
+         <Link href={"/"}><Button >Volver a la Home</Button></Link>
+      </div>
+   )
+}
