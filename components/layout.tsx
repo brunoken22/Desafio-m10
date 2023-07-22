523946043d27bcac9c72513c2519e9aab8fc3496
brunoken22 <bruno_am_22@hotmail.com>
2023-07-22 09:36:47 -0300

    corregido

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,60 +1,54 @@
-"use client" 
-import {ResponsiveAppBar} from "@/components/header"
-import {Footer} from '@/components/footer'
-import styled from 'styled-components'
-import {RecoilRoot} from 'recoil';
-import { ThemeProvider as MuiThemeProvider , createTheme  } from '@mui/material/styles';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
-import { useServerInsertedHTML } from 'next/navigation'
-import { useState } from "react";
+"use client"
+import { useEffect } from 'react';
+import { ResponsiveAppBar } from '@/components/header';
+import { Footer } from '@/components/footer';
+import styled, { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 
 const StyledTheme = {
-   bg: "#121212",
-   color:"#fff"
- };
- 
- const muiTheme = createTheme({
-   
-   palette: {
-      background:{
-         paper:"#121212"
-      },
-      mode:"dark"
+  bg: "#121212",
+  color: "#fff"
+};
+
+const muiTheme = createTheme({
+  palette: {
+    background: {
+      paper: "#121212"
     },
- });
+    mode: "dark"
+  },
+});
 
 const Div = styled.div`
-   padding: 1.5rem;
-   display: grid;
-   row-gap: 5rem;
-`
+  padding: 1.5rem;
+  display: grid;
+  row-gap: 5rem;
+`;
 
-export function MainLayout({children}:any){
-   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
- 
-  useServerInsertedHTML(() => {
-    const styles = styledComponentsStyleSheet.getStyleElement()
-    styledComponentsStyleSheet.instance.clearTag()
-    return <>{styles}</>
-  })
- 
-   return(
-         
-      <RecoilRoot >
-         <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
-            <StyledThemeProvider theme={StyledTheme}>
-               <MuiThemeProvider theme={muiTheme}>
-                  <ResponsiveAppBar/>
-               </MuiThemeProvider> 
-                  <Div>
-                     {children}
-                  </Div>
-               <MuiThemeProvider theme={muiTheme}>
-                  <Footer/>
-               </MuiThemeProvider> 
-            </StyledThemeProvider>
-         </StyleSheetManager>
-      </RecoilRoot>
-   )
+export function MainLayout({ children }: any) {
+  useEffect(() => {
+    // Remove server-side inserted styles on the client-side
+    const styleTags = document.querySelectorAll('style[data-styled-components]');
+    styleTags.forEach((styleTag) => {
+      if (styleTag.getAttribute('data-styled-components')) {
+        styleTag.remove();
+      }
+    });
+  }, []);
+
+  return (
+    <RecoilRoot>
+      <ThemeProvider theme={StyledTheme}>
+        <MuiThemeProvider theme={muiTheme}>
+          <ResponsiveAppBar />
+        </MuiThemeProvider>
+        <Div>{children}</Div>
+        <MuiThemeProvider theme={muiTheme}>
+          <Footer />
+        </MuiThemeProvider>
+      </ThemeProvider>
+    </RecoilRoot>
+  );
 }
